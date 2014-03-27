389855b5b2fb681064105d255185fcaf7992ce77
Muhammad Iqbal CR <boku.wa.kyuu@gmail.com>
2014-03-28 00:34:17 +0800

    Added version support for gnome 3.8

--- extension.js
+++ extension.js
@@ -86,6 +86,7 @@ const QuotaMenu = new Lang.Class({
       let line, count = 0;
       do {
         line = inputStream.read_line(null);
+        // global.log("replyLine: " + line);
         if(/id="tablelist__ctl3_tid[1-3]"/.test(line)) {
           quota.push(parseFloat(/[0-9,]{3,}/.exec(line)[0].replace(/,/gi, "")));
           global.log("NTUST QUOTA " + count + ": " + quota[count]);
@@ -103,10 +104,10 @@ const QuotaMenu = new Lang.Class({
       } else this.setIcon("system-run-symbolic");
 
       // Write to file
-      let fileOutputStream = Gio.file_new_for_path("/var/www/quota").replace(null, false, Gio.FileCreateFlags.PRIVATE, null);
+      // let fileOutputStream = Gio.file_new_for_path("/var/www/quota").replace(null, false, Gio.FileCreateFlags.PRIVATE, null);
       // let jsonOutput = JSON.stringify(quota);
-      fileOutputStream.write(JSON.stringify(quota), null);
-      fileOutputStream.close(null);
+      // fileOutputStream.write(JSON.stringify(quota), null);
+      // fileOutputStream.close(null);
     } catch (ex) {
       global.log("NTUST ERR: " + ex);
       this.setIcon("dialog-error-symbolic");
@@ -137,14 +138,14 @@ const QuotaMenu = new Lang.Class({
               'Cache-Control: max-age=0\r\n' +
               'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n' +
               'Origin: http://netweb.ntust.edu.tw\r\n' +
-              'User-Agent: Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.65 Safari/537.36\r\n' +
+              'User-Agent: Mozilla/5.0 (compatible; NTUST Dormweb checker/0.2; https://github.com/kyuucr/NTUST-Dormweb-Gnome-Shell-Extensions/)\r\n' +
               'Content-Type: application/x-www-form-urlencoded\r\n' +
               'Referer: http://netweb.ntust.edu.tw/dormweb/flowquery.aspx\r\n' +
               'Accept-Encoding: gzip,deflate,sdch\r\n' +
               'Accept-Language: en-US,en;q=0.8,id;q=0.6\r\n' +
-              'Cookie: ASP.NET_SessionId=3bsp2y550tp5eduvup4b2vzh\r\n\r\n',
+              'Cookie: ASP.NET_SessionId=jrhjt545okx02f55uslixr55\r\n\r\n',
 
-  PostBody: '__EVENTTARGET=ipdata&__EVENTARGUMENT=&__VIEWSTATE=dDwtNDY0NjA5NTM4O3Q8O2w8aTwyPjs%%2BO2w8dDw7bDxpPDE%%2BO2k8Mz47aTw1PjtpPDc%%2BO2k8MTM%%2BOz47bDx0PDtsPGk8MT47aTw3PjtpPDk%%2BOz47bDx0PHA8cDxsPFRleHQ7PjtsPOWci%%2Beri%%2BWPsOeBo%%2BenkeaKgOWkp%%2BWtuCDlsI3lpJbntrLot6%%2FmtYHph4%%2Fmn6XoqaI7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPOeZu%%2BWFpeaXpeacn%%2B%%2B8mjIwMTMvMDQvMjk7Pj47Pjs7Pjt0PDtsPGk8MD47PjtsPHQ8O2w8aTwxPjtpPDI%%2BOz47bDx0PDtsPGk8MD47PjtsPHQ8dDw7O2w8aTwwPjs%%2BPjs7Pjs%%2BPjt0PDtsPGk8MT47PjtsPHQ8dDxwPHA8bDxEYXRhVGV4dEZpZWxkOz47bDxTdHJpbmdUZXh0Oz4%%2BOz47dDxpPDQ%%2BO0A8Qnl0ZXM7S0I7TUI7R0I7PjtAPEJ5dGVzO0tCO01CO0dCOz4%%2BOz47Oz47Pj47Pj47Pj47Pj47dDw7bDxpPDc%%2BOz47bDx0PHA8cDxsPFRleHQ7PjtsPFxlOz4%%2BOz47Oz47Pj47dDxwPHA8bDxUZXh0Oz47bDxcPGJyXD7ms6jmhI%%2FvvJrlm6DntbHoqIjln7rmupbkuI3lkIzvvIzoq4vli7%%2FoiIflgIvkurrpm7vohabmr5TovIPvvIzlpoLmnInllY%%2FpoYzoq4ttYWls5YiwIGNjcG9zdG1hbkBtYWlsLm50dXN0LmVkdS50dyDoqI7oq5bjgII7Pj47Pjs7Pjt0PHA8cDxsPFZpc2libGU7PjtsPG88Zj47Pj47PjtsPGk8MT47aTwzPjs%%2BO2w8dDxwPHA8bDxUZXh0Oz47bDzoqbPntLDmtYHph4%%2Fmn6XoqaI7Pj47Pjs7Pjt0PEAwPHA8cDxsPFBhZ2VTaXplOz47bDxpPDMyNzYwPjs%%2BPjs%%2BOzs7Ozs7Ozs7Oz47Oz47Pj47dDxwPHA8bDxWaXNpYmxlOz47bDxvPGY%%2BOz4%%2BOz47bDxpPDE%%2BO2k8NT47aTw3PjtpPDk%%2BOz47bDx0PHA8cDxsPFRleHQ7PjtsPCo7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPFxlOz4%%2BOz47Oz47dDxwPHA8bDxUZXh0Oz47bDwwOz4%%2BOz47Oz47dDxwPHA8bDxUZXh0Oz47bDxcZTs%%2BPjs%%2BOzs%%2BOz4%%2BOz4%%2BOz4%%2BOz5OXN8%%2F29VZ8BTgVn1zF%%2BVW3Jpv6A%%3D%%3D&RB_1=%%E8%%A9%%B3%%E7%%B4%%B0%%E6%%B5%%81%%E9%%87%%8F%%E6%%9F%%A5%%E8%%A9%%A2&un=Bytes&do_date=%s&ipdata=%s&hip=%s&msg_sysadm=%%EF%%BC%%8C%%E8%%AB%%8B%%E6%%92%%A5%%E5%%88%%86%%E6%%A9%%9F6212%%E6%%B4%%BD%%E7%%AE%%A1%%E7%%90%%86%%E8%%80%%85%%EF%%BC%%81&p_ip=%s&dorm=&pdate=%s&page_size=32760&sel_str=dodate%%3D%%27*****%%27&tablename=ipflowtable&tot_columns=4&tot_rows=0&tinsert=&hmaxlen=10%%2C14%%2C14%%2C14%%2C13%%2C10%%2C13%%2C6%%2C6%%2C13%%2C20%%2C15%%2C15%%2C15%%2C15&od_str=&un_v=1',
+  PostBody: '__EVENTTARGET=ipdata&__EVENTARGUMENT=&__VIEWSTATE=dDwtNDY0NjA5NTM4O3Q8O2w8aTwyPjs%%2BO2w8dDw7bDxpPDE%%2BO2k8Mz47aTw1PjtpPDc%%2BO2k8MTM%%2BOz47bDx0PDtsPGk8MT47aTw3PjtpPDk%%2BOz47bDx0PHA8cDxsPFRleHQ7PjtsPOWci%%2Beri%%2BWPsOeBo%%2BenkeaKgOWkp%%2BWtuCDlsI3lpJbntrLot6%%2FmtYHph4%%2Fmn6XoqaIo5qCh5YWn57ay6Lev5bCI55SoKTs%%2BPjs%%2BOzs%%2BO3Q8cDxwPGw8VGV4dDs%%2BO2w855m75YWl5pel5pyf77yaMjAxNC8wMy8yODs%%2BPjs%%2BOzs%%2BO3Q8O2w8aTwwPjs%%2BO2w8dDw7bDxpPDE%%2BO2k8Mj47PjtsPHQ8O2w8aTwwPjs%%2BO2w8dDx0PDs7bDxpPDA%%2BOz4%%2BOzs%%2BOz4%%2BO3Q8O2w8aTwxPjs%%2BO2w8dDx0PHA8cDxsPERhdGFUZXh0RmllbGQ7PjtsPFN0cmluZ1RleHQ7Pj47Pjt0PGk8ND47QDxCeXRlcztLQjtNQjtHQjs%%2BO0A8Qnl0ZXM7S0I7TUI7R0I7Pj47Pjs7Pjs%%2BPjs%%2BPjs%%2BPjs%%2BPjt0PDtsPGk8Nz47PjtsPHQ8cDxwPGw8VGV4dDs%%2BO2w8XGU7Pj47Pjs7Pjs%%2BPjt0PHA8cDxsPFRleHQ7PjtsPFw8YnJcPuazqOaEj%%2B%%2B8muWboOe1seioiOWfuua6luS4jeWQjO%%2B8jOiri%%2BWLv%%2BiIh%%2BWAi%%2BS6uumbu%%2BiFpuavlOi8g%%2B%%2B8jOWmguacieWVj%%2BmhjOiri21haWzliLAgY2Nwb3N0bWFuQG1haWwubnR1c3QuZWR1LnR3IOiojuirluOAgjs%%2BPjs%%2BOzs%%2BO3Q8cDxwPGw8VmlzaWJsZTs%%2BO2w8bzxmPjs%%2BPjs%%2BO2w8aTwxPjtpPDM%%2BOz47bDx0PHA8cDxsPFRleHQ7PjtsPOips%%2Be0sOa1gemHj%%2Bafpeipojs%%2BPjs%%2BOzs%%2BO3Q8QDA8cDxwPGw8UGFnZVNpemU7PjtsPGk8MzI3NjA%%2BOz4%%2BOz47Ozs7Ozs7Ozs7Pjs7Pjs%%2BPjt0PHA8cDxsPFZpc2libGU7PjtsPG88Zj47Pj47PjtsPGk8MT47aTw1PjtpPDc%%2BO2k8OT47PjtsPHQ8cDxwPGw8VGV4dDs%%2BO2w8Kjs%%2BPjs%%2BOzs%%2BO3Q8cDxwPGw8VGV4dDs%%2BO2w8XGU7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPDA7Pj47Pjs7Pjt0PHA8cDxsPFRleHQ7PjtsPFxlOz4%%2BOz47Oz47Pj47Pj47Pj47PjWOIK06sfz2utthGVxiENPZeg6Z&RB_1=%%E8%%A9%%B3%%E7%%B4%%B0%%E6%%B5%%81%%E9%%87%%8F%%E6%%9F%%A5%%E8%%A9%%A2&un=Bytes&do_date=%s&ipdata=%s&Bbase=%%E6%%9F%%A5%%E8%%A9%%A2&hip=%s&msg_sysadm=%%EF%%BC%%8C%%E8%%AB%%8B%%E6%%92%%A5%%E5%%88%%86%%E6%%A9%%9F6212%%E6%%B4%%BD%%E7%%AE%%A1%%E7%%90%%86%%E8%%80%%85%%EF%%BC%%81&p_ip=%s&dorm=&pdate=%s&page_size=32760&sel_str=dodate%%3D%%27*****%%27&tablename=ipflowtable&tot_columns=4&tot_rows=0&tinsert=&hmaxlen=10%%2C14%%2C14%%2C14%%2C13%%2C10%%2C13%%2C6%%2C6%%2C13%%2C20%%2C15%%2C15%%2C15%%2C15&od_str=&un_v=1',
 
   destroy: function() {
     this.parent();
